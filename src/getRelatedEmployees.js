799a4e5470919bac2a05535d72f7143f1e885f19
Angelo Fonseca <angeloniemeyer97@gmail.com>
2024-03-07 12:47:05 -0300

    Requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,17 @@
 const data = require('../data/zoo_data');
 
 const isManager = (id) => {
-  // seu código aqui
+  const findManager = data.employees.find((employee) => employee.managers.includes(id));
+  if (findManager) return true;
+  return false;
 };
 
 const getRelatedEmployees = (managerId) => {
-  // seu código aqui
+  if (isManager(managerId)) {
+    const result = data.employees.filter((employee) => employee.managers.includes(managerId));
+    return result.map((employee) => `${employee.firstName} ${employee.lastName}`);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 };
 
 module.exports = { isManager, getRelatedEmployees };
