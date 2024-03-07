5db8800ae820fb6fdf601a5f883f8053e956ca1e
Angelo Fonseca <angeloniemeyer97@gmail.com>
2024-03-07 16:47:25 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
 
 const countAnimals = (animal) => {
-  // seu código aqui
-};
+  if (!animal) {
+    return data.species.reduce((acc, specie) => {
+      acc[specie.name] = specie.residents.length;
+      return acc;
+    }, {});
+  }
+  const parameterArray = Object.values(animal);
+  const getAnimalBySpecies = data.species
+    .find((specie) => parameterArray
+      .includes(specie.name)).residents;
 
+  const getAnimalBySpeciesAndGender = getAnimalBySpecies
+    .filter((resident) => parameterArray
+      .includes(resident.sex));
+
+  if (parameterArray.length === 1) return getAnimalBySpecies.length;
+  if (parameterArray.length === 2) return getAnimalBySpeciesAndGender.length;
+};
 module.exports = countAnimals;
