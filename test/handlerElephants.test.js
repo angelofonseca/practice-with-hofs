6b0b883ee0b3e1ff5822b9c832f45187b6ef7684
Angelo Fonseca <angeloniemeyer97@gmail.com>
2024-03-07 17:26:53 -0300

    Requisito 6

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,53 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('retorna a quantidade de elefantes', () => {
+    const actual = handlerElephants('count');
+    const expected = 4;
+    expect(actual).toEqual(expected);
+  });
+  it('retorna um array com a relação dos nomes de todos os elefantes', () => {
+    const actual = handlerElephants('names');
+    const expected = ['Ilana', 'Orval', 'Bea', 'Jefferson'];
+    expect(actual).toEqual(expected);
+    expect(actual.length).toBe(4);
+    expect(actual.includes('Jefferson')).toBeTruthy();
+  });
+  it('retorna a média de idade dos elefantes', () => {
+    const actual = handlerElephants('averageAge');
+    const expected = 10.5;
+    expect(actual).toEqual(expected);
+  });
+  it('retorna a localização dos elefantes dentro do Zoológico', () => {
+    const actual = handlerElephants('location');
+    const expected = 'NW';
+    expect(actual).toBe(expected);
+  });
+  it('retorna a popularidade dos elefantes', () => {
+    const actual = handlerElephants('popularity');
+    const expected = 5;
+    expect(actual).toBe(expected);
+  });
+  it('retorna um array com a relação de dias em que é possível visitar os elefantes', () => {
+    const actual = handlerElephants('availability');
+    const expected = ['Friday', 'Saturday', 'Sunday', 'Tuesday'];
+    expect(actual).toEqual(expected);
+    expect(actual.length).toEqual(4);
+    expect(actual.includes('Saturday')).toBeTruthy();
+  });
+  it('testa se o parametro passado é nulo', () => {
+    const actual = handlerElephants();
+    const expected = undefined;
+    expect(actual).toBe(expected);
+  });
+  it('testa se o parametro passado não é uma string', () => {
+    const actual = handlerElephants(10);
+    const expected = 'Parâmetro inválido, é necessário uma string';
+    expect(actual).toBe(expected);
+  });
+  it('testa se o parametro passado existe', () => {
+    const actual = handlerElephants('popularidade');
+    const expected = null;
+    expect(actual).toBe(expected);
+  });
+});
