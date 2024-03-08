a19b5d5ab84b6d676dcd3b4c8745c1d09d7d2f26
Angelo Fonseca <angeloniemeyer97@gmail.com>
2024-03-08 11:14:47 -0300

    erro de linter em calculateEntry

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,17 +1,15 @@
 const data = require('../data/zoo_data');
 
-const countEntrants = (entrants) => {
-  return entrants.reduce((acc, entrant) => {
-    if (entrant.age < 18) {
-      acc.child += 1;
-    } else if (entrant.age > 17 && entrant.age < 50) {
-      acc.adult += 1;
-    } else if (entrant.age > 49) {
-      acc.senior += 1;
-    }
-    return acc;
-  }, { child: 0, adult: 0, senior: 0 });
-};
+const countEntrants = (entrants) => entrants.reduce((acc, entrant) => {
+  if (entrant.age < 18) {
+    acc.child += 1;
+  } else if (entrant.age > 17 && entrant.age < 50) {
+    acc.adult += 1;
+  } else if (entrant.age > 49) {
+    acc.senior += 1;
+  }
+  return acc;
+}, { child: 0, adult: 0, senior: 0 });
 
 const calculateEntry = (entrants) => {
   if (!entrants) return 0;
